c5d1eb1ffef009c856a42797eaa7475b2d7f0664
Romal-2303 <angryrocket789@gmail.com>
2025-05-18 17:06:27 +0530

    health check for testing

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routers import mock_summary
 from routers import pdf_info
 from routers import video_info
+from routers import heath
 
 
 app = FastAPI()
@@ -10,3 +11,4 @@ app = FastAPI()
 app.include_router(mock_summary.router)
 app.include_router(pdf_info.router)
 app.include_router(video_info.router)
+app.include_router(heath.router)
